785aa2a60edd8908d7744dbb2c1609f506d0ff02
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-09-08 20:07:57 -0400

    Added interpolation of data and color

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import styles from "./MainMap.module.css";
 import ReactMapGL, { Source, Layer } from "react-map-gl";
-import 'mapbox-gl/dist/mapbox-gl.css';
-
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const MainMap = () => {
-  // Mapbox 
+  // Mapbox
   const mapApiToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
   const [viewport, setViewport] = useState({
@@ -20,17 +19,21 @@ const MainMap = () => {
   const [centroidData, setCentroidData] = useState(null);
   const [footprintData, setFootprintData] = useState(null);
 
-   // Load GeoJSON files when component mounts
-   useEffect(() => {
+  // Load GeoJSON files when component mounts
+  useEffect(() => {
     // Load Centroids GeoJSON
-    fetch("/data/NASA_GEO/375m_Fire_Detection_Centroids_(Last_0_to_6hrs).geojson")
-      .then(response => response.json())
-      .then(data => setCentroidData(data));
+    fetch(
+      "/data/NASA_GEO/375m_Fire_Detection_Centroids_(Last_0_to_6hrs).geojson"
+    )
+      .then((response) => response.json())
+      .then((data) => setCentroidData(data));
 
     // Load Footprints GeoJSON
-    fetch("/data/NASA_GEO/375m_Fire_Detection_Footprints_(Last_0_to_6hrs).geojson")
-      .then(response => response.json())
-      .then(data => setFootprintData(data));
+    fetch(
+      "/data/NASA_GEO/375m_Fire_Detection_Footprints_(Last_0_to_6hrs).geojson"
+    )
+      .then((response) => response.json())
+      .then((data) => setFootprintData(data));
   }, []);
 
   return (
@@ -41,6 +44,7 @@ const MainMap = () => {
           mapboxAccessToken={mapApiToken}
           mapStyle="mapbox://styles/mapbox/dark-v10"
           onMove={(evt) => setViewport(evt.viewState)}
+          projection="globe"
         >
           {/* Add the footprints as a fill layer */}
           {footprintData && (
@@ -49,8 +53,16 @@ const MainMap = () => {
                 id="footprints-layer"
                 type="fill"
                 paint={{
-                  'fill-color': '#FF4500', // Orange color for fire footprints
-                  'fill-opacity': 0.5
+                  "fill-color": [
+                    "interpolate",
+                    ["linear"],
+                    ["to-number", ["slice", ["get", "FRP"], 0, -3]],
+                    0, "#FFFF99", // Yellow color for low FRP
+                    5,"#FFFF00", // Yellow color for low FRP
+                    10,"#FFA500", // Orange color for medium FRP
+                    30,"#FF0000", // Red color for high FRP
+                  ],
+                  "fill-opacity": 0.3,
                 }}
               />
             </Source>
@@ -63,8 +75,17 @@ const MainMap = () => {
                 id="centroids-layer"
                 type="circle"
                 paint={{
-                  'circle-radius': 6,
-                  'circle-color': '#FF0000' // Red color for fire centroids
+                  "circle-radius": 6,
+                  "circle-color": [
+                    "interpolate",
+                    ["linear"],
+                    ["to-number", ["slice", ["get", "FRP"], 0, -3]],
+                    0, "#FFFF99", // Yellow color for low FRP
+                    5,"#FFFF00", // Yellow color for low FRP
+                    10,"#FFA500", // Orange color for medium FRP
+                    30,"#FF0000", // Red color for high FRP
+                  ],
+                  "circle-opacity": 0.7,
                 }}
               />
             </Source>
