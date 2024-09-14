6f183891a8bc215d6ad83fb2664c016f54c12d13
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-09-14 13:54:40 -0400

    Added address building and other fields to fire station popups

--- src/components/FireStationPopup.jsx
+++ src/components/FireStationPopup.jsx
@@ -1,12 +1,36 @@
 
 const FireStationPopup = (props) => {
     const fireStation = props.fireStation;
+    
+    const buildAddress = (fireStation) => {
+        let address = "";
+
+        // Start building the address
+        address += fireStation["addr:housenumber"] + " " + fireStation["addr:street"];
+
+        if (fireStation["addr:city"]){
+            address += ", " + fireStation["addr:city"];
+        }
+
+        if (fireStation["addr:state"]){
+            address += ", " + fireStation["addr:state"];
+        } else if (fireStation["addr:province"]){
+            address += ", " + fireStation["addr:province"];
+        }
+
+        return address;
+
+    }
     return (
         <div>
             <h2>Fire Station</h2>
             <p>Latitude: {fireStation.latitude}</p>
             <p>Longitude: {fireStation.longitude}</p>
-            {fireStation.name && <p>Name: {fireStation.name}</p>}
+            {fireStation.official_name && <p>Name: {fireStation.official_name}</p>}
+            {!fireStation.official_name && fireStation.name && <p>Name: {fireStation.name}</p>}
+            {fireStation["addr:housenumber"] && fireStation["addr:street"] && <p>Address: {buildAddress(fireStation)}</p>}
+            {fireStation.phone && <p>Phone: {fireStation.phone}</p>}
+            {fireStation.operator && <p>Operator: {fireStation.operator}</p>}
         </div>
     );
 }
