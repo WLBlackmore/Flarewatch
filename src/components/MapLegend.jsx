97dfc046e75f52196f07a3145e79a7bd15c712d6
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-10-15 19:14:34 -0400

    Add FRP legend markers

--- src/components/MapLegend.jsx
+++ src/components/MapLegend.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./MapLegend.module.css";
+import FrpLegend from "./legends/FrpLegend";
 
 const MapLegend = () => {
   return (
     <div className={styles.legend}>
       <h1>Map Legend</h1>
+      <FrpLegend />
     </div>
   );
 };
--- src/components/legends/FrpLegend.jsx
+++ src/components/legends/FrpLegend.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import styles from "./FrpLegend.module.css";
 
 const FrpLegend = () => {
-    <>
-        
-    </>
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.high} />
+      <div className={styles.medium} />
+      <div className={styles.low} />
+    </div>
+  );
+};
 
 export default FrpLegend;
