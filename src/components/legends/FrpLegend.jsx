d31ec157bd76bfe45f106a645c00081036cfd9b4
WLBlackmore <william.blackmore@mail.mcgill.ca>
2024-10-15 20:01:39 -0400

    adjust interpolation for FPR and legend

--- src/components/legends/FrpLegend.jsx
+++ src/components/legends/FrpLegend.jsx
@@ -7,15 +7,15 @@ const FrpLegend = () => {
         <h3>Fire Radiative Power</h3>
       <div className={styles.legendItem}>
         <div className={styles.high} />
-        <p>High</p>
+        <p>High {"(> 10 MW)"}</p>
       </div>
       <div className={styles.legendItem}>
         <div className={styles.medium} />
-        <p>Medium</p>
+        <p>Medium {"(2-10 MW)"}</p>
       </div>
       <div className={styles.legendItem}>
         <div className={styles.low} />
-        <p>Low</p>
+        <p>Low {"(< 2 MW)"}</p>
       </div>
     </div>
   );
